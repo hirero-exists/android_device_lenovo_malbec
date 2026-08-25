179da9807159312c53757b35b254aa33dbabd7c3
hirero-exists <hirerokazuoa@gmail.com>
2026-08-25 15:17:00 +0530

    malbec: classify NVT touch as internal, extract bluetooth HAL
    The touchscreen was detected but every touch was dropped because
    EventHub had no AssociatedDisplay mapping without an internal-device
    idc file, and droid.bluetooth aborted waiting for IBluetoothHci/default
    because the QTI HAL binary and its implementation libs were never
    extracted. Add the idc classification and the missing bluetooth
    closure.
    
    Change-Id: Ib4d7d56d60e6f9f430a16485ae32de690018ba3f
    Signed-off-by: hirero-exists <hirerokazuoa@gmail.com>

--- device.mk
+++ device.mk
@@ -117,6 +117,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/usr/idc/NVTCapacitiveTouchScreen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/NVTCapacitiveTouchScreen.idc \
+    $(LOCAL_PATH)/usr/idc/NVTCapacitivePen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/NVTCapacitivePen.idc
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl
